f0a97989557e26b6ae9ed42e538b0192b9993709
BlackJackSPB <Rystzov1995@yandex.ru>
2025-08-03 22:53:27 +0300

    step 3

--- page_analyzer/models.py
+++ page_analyzer/models.py
@@ -81,3 +81,26 @@ def get_all_urls():
         if conn:
             conn.close()
     return urls_data
+
+
+def init_db():
+    conn = get_db_connection()
+    try:
+        with conn.cursor() as cur:
+            cur.execute("""
+                CREATE TABLE IF NOT EXISTS urls (
+                    id serial PRIMARY KEY,
+                    name varchar(255) NOT NULL,
+                    created_at date NOT NULL
+                );
+            """)
+            conn.commit()
+            print("✅ Таблица 'urls' создана или уже существует.")
+    except psycopg.Error as e:
+        print(f"❌ Ошибка при создании таблицы: {e}")
+        conn.rollback()
+    finally:
+        conn.close()
+
+if __name__ == "__main__":
+    init_db()
