eecb1b2f79791db8a266aa7fcff95bef5b6b9d47
Rystzov <rystzov1995@yandex.ru>
2025-08-03 18:57:17 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,21 +1,7 @@
-set -o errexit
-
-echo "--- Installing uv (Build Step) ---"
-
 curl -LsSf https://astral.sh/uv/install.sh | sh
 
-source $HOME/.local/bin/env
-
-uv --version
-
-echo "--- Installing project dependencies using make install (Build Step) ---"
+. "$HOME/.local/bin/env"
 
 make install
 
-echo "--- Running database migrations (Build Step) ---"
-
-DATABASE_URL из переменных окружения Render
-
-psql -a -d $DATABASE_URL -f database.sql
-
-echo "--- Build finished ---"
+psql -a -d "$DATABASE_URL" -f database.sql
